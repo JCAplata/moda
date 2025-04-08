dd4ca892ad7a18a36b6c8eccc927c57fbc40d52f
camilo.amezquita <jcaopita@gmail.com>
2025-04-07 23:44:12 -0500

    completado

--- contenido/js/carrusel.js
+++
@@ -1,7 +0,0 @@
-let index = 0;
-function moveSlide() {
-    const slides = document.querySelectorAll(".carousel-container img");
-    index = (index + 1) % slides.length;
-    document.querySelector(".carousel-container").style.transform = `translateX(-${index * 100}%)`;
-}
-setInterval(moveSlide, 2000);
